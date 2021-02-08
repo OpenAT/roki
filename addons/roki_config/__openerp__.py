7909e9fda3773de6e51dc90855cfb3805154d2fd
Michael Karrer <michaelkarrer81@gmail.com>
2021-02-08 17:44:09 +0100

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/roki_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'roki_config',
+    'summary': '''FS-Online roki instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance roki
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
